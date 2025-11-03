987109cfb7e4de8056598d27019d03db969bd4cb
nishatdbdlt <nishat7453@gmail.com>
2025-11-03 22:17:39 +0600

    working

--- nishat.py
+++ nishat.py
@@ -1,3 +1,4 @@
+#1
 import re
 
 def count_unique_words(text):
@@ -8,3 +9,42 @@ def count_unique_words(text):
 
 text = "Bangladesh is a beautiful country. Bangladesh has rivers."
 print("Unique words:", count_unique_words(text))
+
+#2
+import string
+
+text = "Bangladesh is rich Country"
+
+# punctuation remove
+translator = str.maketrans('', '', string.punctuation)
+clean_text = text.translate(translator)
+
+# lowercase
+clean_text = clean_text.lower()
+
+# split into words
+words = clean_text.split()
+
+# print the word list
+print(words)
+#3
+import string
+
+text =" bangladesh is richcountry"
+
+translator=str.maketrans('','',string.punctuation)
+clean_text =text.translate(translator)
+clean_text=clean_text.lower()
+
+words=clean_text.split()
+print(words)
+
+
+import string
+text ="india is bainchod country"
+
+translator=str.maketrans('','',string.punctuation)
+clean_text=text.translate(translator)
+clean_text=clean_text.lower()
+words=clean_text.split()
+print(words)
